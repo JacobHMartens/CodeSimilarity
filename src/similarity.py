e9f5d3f36c695a17a21b76dd8b51c8cc5bf9fac4
Jacob Martens <jacobhmartens@gmail.com>
2025-06-14 15:01:35 +0200

    refac: Optimized sim matrix generation with numpy

--- src/similarity.py
+++ src/similarity.py
@@ -14,7 +14,6 @@ def _complenght(data: bytes, compressor: compFunc) -> int:
     """
     return len(compressor(data))
 
-
 def sim_C_NCD(files: list[File], compressor: compFunc) -> np.ndarray:
     """
     Computes the pairwise similarity of a list of files using Normalized Compression Distance (NCD) with the specified compressor.
@@ -22,23 +21,34 @@ def sim_C_NCD(files: list[File], compressor: compFunc) -> np.ndarray:
     Second parameter is a compressor function.
     Returns a list of tuples containing the names of the compared files and their similarity score.
     """
-    def NCD(x: bytes, y: bytes, compressor: compFunc) -> float:
-        xy = x + y
-        partial_compressed_size = partial(_complenght, compressor=compressor)
-        Zx, Zy, Zxy = list(map(partial_compressed_size, [x, y, xy]))
-        return (Zxy - min(Zx, Zy)) / max(Zx, Zy)
     
-    size = len(files)
-    similarities = np.zeros((size, size), dtype=float)
-    for i in range(len(files)):
-        file1 = files[i]
-        for j in range(i, len(files)):
-            file2 = files[j]
-            sim = 1 - NCD(file1.get_bytes(), file2.get_bytes(), compressor)
-            similarities[i, j] = sim
-            if i != j:
-                similarities[j, i] = sim
-    return similarities
+    def get_all_compressed_lengths(files: list[File], compressor: compFunc):
+        """
+        Returns a dictionary mapping file names to their compressed lengths. 
+        Includes all pairwise combinations.
+        """
+        compressed_file_lengths = list(map(lambda file: _complenght(file.get_bytes(), compressor), files))
+        compressed_pair_lengths = np.zeros((len(files), len(files)), dtype=int)
+        
+        for i in range(len(files)):
+            for j in range(i, len(files)):
+                file_i, file_j = files[i], files[j]
+                compressed_pair_lengths[i, j] = _complenght(file_i.get_bytes() + file_j.get_bytes(), compressor)
+        compressed_pair_lengths += compressed_pair_lengths.T - np.diag(np.diag(compressed_pair_lengths))  # Make it symmetric (copy upper triangle to lower triangle)
+        return compressed_file_lengths, compressed_pair_lengths
+        
+    def generate_similarity_matrix(compressed_file_lengths, compressed_pair_lengths):
+        """
+        Generates a similarity matrix based on NCD.
+        """
+        max_compressed_length_matrix = np.maximum.outer(compressed_file_lengths, compressed_file_lengths)
+        min_compressed_length_matrix = np.minimum.outer(compressed_file_lengths, compressed_file_lengths)
+        
+        sim_matrix = 1 - (compressed_pair_lengths - min_compressed_length_matrix) / max_compressed_length_matrix
+        return sim_matrix
+    
+    compressed_file_lengths, compressed_pair_lengths = get_all_compressed_lengths(files, compressor)
+    return generate_similarity_matrix(compressed_file_lengths, compressed_pair_lengths)
 
 def sim_C_ICD(files: list[File], compressor: compFunc) -> np.ndarray:
     """
